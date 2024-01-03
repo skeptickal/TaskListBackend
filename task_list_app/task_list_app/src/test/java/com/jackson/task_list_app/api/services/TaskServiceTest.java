a92144e37b039e2305bb9afd541e222a5a5fc776
Jackson Geer <jacksongeer@gmail.com>
2024-01-03 01:32:38 -0500

    added tests

--- task_list_app/task_list_app/src/test/java/com/jackson/task_list_app/api/services/TaskServiceTest.java
+++ task_list_app/task_list_app/src/test/java/com/jackson/task_list_app/api/services/TaskServiceTest.java
@@ -1,1 +1,59 @@
 package com.jackson.task_list_app.api.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.jackson.task_list_app.api.models.MyTask;
+import com.jackson.task_list_app.api.repository.TaskRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
+
+    @InjectMocks
+    private TaskService taskService;
+
+    @Mock
+    private TaskRepository mockTaskRepository;
+
+    @Test
+    void testUpdateMyTask() {
+        when(mockTaskRepository.findById(anyLong())).thenReturn(Optional.of(new MyTask(1L, "do the dishes")));
+        when(mockTaskRepository.save(any(MyTask.class))).thenReturn(new MyTask(2L, "wash the dishes"));
+
+        MyTask actual = taskService.updateMyTask(new MyTask(1L, "wash the dishes"));
+        verify(mockTaskRepository).findById(1L);
+
+        assertEquals("wash the dishes", actual.getName());
+    }
+
+    @Test
+    void testUpdateMyTaskWhenAbsent() {
+        when(mockTaskRepository.findById(anyLong())).thenReturn(Optional.ofNullable(null));
+
+        MyTask actual = taskService.updateMyTask(new MyTask(1L, "wash the dishes"));
+
+        assertEquals(null, actual);
+    }
+
+     @Test
+    void testUpdateMyTaskThrows() {
+
+        when(mockTaskRepository.findById(anyLong())).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> taskService.updateMyTask(new MyTask(3L, "throw an exception")));
+    }
+
+
+}
