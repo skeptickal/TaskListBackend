53b41602901dbb99d4978a0af3adf58f240b5657
Jackson Geer <jacksongeer@gmail.com>
2024-01-03 17:56:28 -0500

    added more controller tests

--- task_list_app/task_list_app/src/test/java/com/jackson/task_list_app/api/controller/TaskControllerTest.java
+++ task_list_app/task_list_app/src/test/java/com/jackson/task_list_app/api/controller/TaskControllerTest.java
@@ -1,6 +1,8 @@
 package com.jackson.task_list_app.api.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -46,10 +48,55 @@ public class TaskControllerTest {
     void testGetTasks() throws Exception {
        List<MyTask> myTasks = new ArrayList<>();
        myTasks.add(new MyTask(1L, "do the dishes")); 
+
        RequestBuilder request = MockMvcRequestBuilders.get("/tasks").accept(MediaType.APPLICATION_JSON);
+
        when(mockTaskService.getTask()).thenReturn(myTasks);
+
        MvcResult response = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
+       
        verify(mockTaskService, times(1)).getTask();
        assertEquals(new ObjectMapper().writeValueAsString(myTasks), response.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testCreateMyTask() throws Exception {
+       List<MyTask> myTasks = new ArrayList<>();
+       myTasks.add(new MyTask(1L, "do the dishes"));
+       String requestJson = new ObjectMapper().writeValueAsString(myTasks.get(0)); 
+       RequestBuilder request = MockMvcRequestBuilders
+       .post("/tasks")
+       .contentType(MediaType.APPLICATION_JSON)
+       .content(requestJson)
+       .accept(MediaType.APPLICATION_JSON);
+
+       when(mockTaskService.createMyTask(any(MyTask.class))).thenReturn(myTasks.get(0));
+
+       MvcResult response = mockMvc.perform(request).andExpect(status().isCreated()).andReturn();
+
+       verify(mockTaskService, times(1)).createMyTask(any(MyTask.class));
+       assertEquals(requestJson, response.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testUpdateMyTask() throws Exception {
+       List<MyTask> myTasks = new ArrayList<>();
+       myTasks.add(new MyTask(1L, "do the dishes"));
+
+       String requestJson = new ObjectMapper().writeValueAsString(myTasks.get(0)); 
+
+       RequestBuilder request = MockMvcRequestBuilders
+       .put("/tasks/{id}", myTasks.get(0).getId())
+       .contentType(MediaType.APPLICATION_JSON)
+       .content(requestJson)
+       .accept(MediaType.APPLICATION_JSON);
+
+       when(mockTaskService.updateMyTask(any(MyTask.class))).thenReturn(myTasks.get(0));
+       when(mockTaskService.getTaskById(anyLong())).thenReturn(myTasks.get(0));
+
+       MvcResult response = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
+
+       verify(mockTaskService, times(1)).updateMyTask(any(MyTask.class));
+       assertEquals(requestJson, response.getResponse().getContentAsString());
     }
 }
